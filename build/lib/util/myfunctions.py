a9541d636f3d6b02fc249fc4ce7814f22650d680
lkesich <lydiarosekesich@gmail.com>
2025-02-14 16:18:19 -0500

    switch to pyproject.toml from setup.py

--- build/lib/util/myfunctions.py
+++
@@ -1,5 +0,0 @@
-from typing import List, Callable
-from functools import reduce
-
-def chain_operations(arg, order_of_operations: List[Callable]):
-    return reduce(lambda x, f: f(x), order_of_operations, arg)
--- tests/test_myfunctions.py
+++ tests/test_myfunctions.py
@@ -1,6 +1,10 @@
-from util import myfunctions
-def test_chain_operations():
-    assert myfunctions.chain_operations(
-        ' abc ', 
-        [str.upper, str.trim]
-    ) == 'ABC'
+import unittest
+from src.utils import myfunctions
+
+class TestSimple(unittest.TestCase):
+    def test_chain_operations(self):
+        result = myfunctions.chain_operations(' abc ', [str.upper, str.trim])
+        self.assertEqual(result, 'ABC')
+
+if __name__ == '__main__':
+    unittest.main()
