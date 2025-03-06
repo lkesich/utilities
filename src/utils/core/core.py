e761704fa2cf0d54ab83b8012714beb193d541c4
lkesich <lydiarosekesich@gmail.com>
2025-03-06 15:56:28 -0500

    add core.flatten_nested_list

--- src/utils/core/core.py
+++ src/utils/core/core.py
@@ -2,11 +2,13 @@ __docformat__ = 'google'
 
 __all__ = [
     'chain_operations',
-    'create_surrogate_key'
+    'create_surrogate_key',
+    'flatten_nested_list'
 ]
 
-from typing import List, Callable
+from typing import List, Callable, Any
 from functools import reduce
+from itertools import chain
 import re
 
 def chain_operations(arg, order_of_operations: List[Callable]):
@@ -49,3 +51,26 @@ def create_surrogate_key(fields: List, delimiter='_') -> str:
     elements = map(lambda e: re.sub('[^a-zA-Z0-9]', '', str(e)), elements)
     id_string = f'{delimiter}'.join(list(elements))
     return id_string
+
+def flatten_nested_list(items: List[Any|List[Any]]) -> List:
+    """Flatten lists with nested elements of arbitrary depths.
+
+    Args:
+        items: List to flatten
+    
+    Returns:
+        Flattened list
+
+    Examples:
+        >>> flatten_nested_list(['a', ['b'], 'c'])
+        ['a','b','c']
+        >>> flatten_nested_list([1, [2], [[3], [4]]])
+        [1, 2, 3, 4]
+    """
+    return list(
+        chain.from_iterable(
+            [item] if not isinstance(item, list)
+            else flatten_nested_list(item)
+            for item in items
+            )
+        )
--- tests/test_core.py
+++ tests/test_core.py
@@ -15,5 +15,18 @@ class TestCreateSurrogateKey(unittest.TestCase):
         result = core.create_surrogate_key([12, 'a.b', None, 'c d'], '~')
         self.assertEqual(result, '12~ab~cd')
 
+class TestFlattenNestedList(unittest.TestCase):
+    def test_one_level_list(self):
+        result = core.flatten_nested_list(['a','b',['c']])
+        self.assertEqual(result, ['a', 'b', 'c'])
+
+    def test_multi_level_list(self):
+        result = core.flatten_nested_list([1, [2], [[3], 4]])
+        self.assertEqual(result, [1, 2, 3, 4])
+
+    def test_mixed_type_list(self):
+        result = core.flatten_nested_list([None, [[False]], [1], 'a'])
+        self.assertEqual(result, [None, False, 1, 'a'])
+
 if __name__ == '__main__':
     unittest.main()
